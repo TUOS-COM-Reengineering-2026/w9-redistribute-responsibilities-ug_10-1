41c11dfb7b0ab4223a9a9a777512c4ec480ac4b1
imoqen <irpemb@gmail.com>
2026-04-28 13:26:07 +0100

    feat: (wip) work on breaking up the Bank god class. focus has been on delegating to Branch.py

--- Bank.py
+++ Bank.py
@@ -4,6 +4,18 @@ from Customer import Customer
 from Payroll import Payroll
 from Staff import Staff
 
+# Account: data container for an account
+# Branch: data container for a branch
+# Customer: data container for a customer
+# Payroll: a payroll
+# PaySchedule: used internally in Payroll
+# Staff: staff "don't edit this class"
+
+# Aim: determine which of the Bank methods belong in the data classes
+# (Account, Branch, or Customer).
+
+# Branch refactoring: moved setup_branch, close_branch, and transfer_staff_member to Branch
+# Moved opening time value for Branch into the class itself.
 
 class Bank:
     def __init__(self):
@@ -12,23 +24,27 @@ class Bank:
         self.customer_addresses = {}  # key: customer, value: address
         self.customer_phone_numbers = {}  # key: customer, value: phone number
         self.branches = []
-        self.branch_opening_times = {}  # key: branch, value: opening time
+        # moved to Branch: self.branch_opening_times = {}  # key: branch, value: opening time
         self.payroll = None
 
+    # Altered by moving opening time to the Branch class
     def setup_branch(self, branch: Branch):
+        branch.set_opening_time("9:00")
         self.branches.append(branch)
-        self.branch_opening_times[branch] = "9:00"  # default opening time
 
-    def close_branch(self, branch: Branch, transfer_branch: Branch):
-        for staff in branch.get_staff():
-            self.transfer_staff_member(branch, transfer_branch, staff)
+    # Calls the delegated method and removes from the list:
+    def close_branch(self, branch: Branch, transfer_branch: Branch): # Branch
+        branch.close_branch(transfer_branch)
         self.branches.remove(branch)
 
-    def transfer_staff_member(self, from_branch: Branch, to_branch: Branch, staff: Staff):
-        from_branch.get_staff().remove(staff)
-        to_branch.get_staff().append(staff)
+    # Removed as can just use set_opening_time instead on the branch.
+    # Otherwise, it doesnt make logical sense to be in the Bank class; would be a static method.
+    #def change_branch_opening_time(self, branch: Branch, time: str): # Branch
+    #    self.branch_opening_times[branch] = time
 
-    def setup_new_account(self, account: Account, customer: Customer):
+    # Everything above has been refactored, rest will do later:
+
+    def setup_new_account(self, account: Account, customer: Customer): # Account or Customer? maybe dont move.
         account.set_customer(customer)
         self.accounts.append(account)
 
@@ -37,30 +53,28 @@ class Bank:
             self.customer_addresses[customer] = "NO ADDRESS"  # default address
             self.customer_phone_numbers[customer] = "NO PHONE NUMBER"  # default phone number
 
-    def obtain_balance(self, account: Account):
+    def obtain_balance(self, account: Account):# Account
         return account.get_balance()
 
-    def add_interest(self, account: Account):
+    def add_interest(self, account: Account): # Account
         balance = account.get_balance()
         interest_rate = account.get_interest_rate()
         interest = balance * interest_rate
         account.set_balance(balance + interest)
 
-    def add_funds(self, account: Account, amount: float):
+    def add_funds(self, account: Account, amount: float): # Account
         balance = account.get_balance()
         account.set_balance(balance + amount)
 
-    def close_account(self, account: Account):
+    def close_account(self, account: Account): # Account
         account.set_customer(None)
         account.set_balance(0)
         self.accounts.remove(account)
 
-    def add_staff_member(self, branch: Branch, staff: Staff):
+    def add_staff_member(self, branch: Branch, staff: Staff): # Branch
         branch.get_staff().append(staff)
 
-    def change_opening_time(self, branch: Branch, time: str):
-        self.branch_opening_times[branch] = time
 
-    def change_payroll_date(self, payroll: Payroll, date: str, staff_category: str):
+    def change_payroll_date(self, payroll: Payroll, date: str, staff_category: str): # maybe Payroll?
         self.payroll = payroll
         self.payroll.get_staff_category_pay_schedule(staff_category).set_pay_date(date)
--- Branch.py
+++ Branch.py
@@ -6,6 +6,7 @@ class Branch:
     def __init__(self, location):
         self._location = location
         self._staff = []
+        self._opening_time = ""
 
     def get_location(self):
         return self._location
@@ -15,3 +16,17 @@ class Branch:
 
     def get_staff(self):
         return self._staff
+
+    def set_opening_time(self, opening_time):
+        self._opening_time = opening_time
+
+    def get_opening_time(self):
+        return self._opening_time
+
+    def close_branch(self, transfer_branch): # Branch
+        for staff in self.get_staff():
+            self.transfer_staff_member(transfer_branch, staff)
+
+    def transfer_staff_member(self, to_branch, staff: Staff): # Branch
+        self.get_staff().remove(staff)
+        to_branch.get_staff().append(staff)
